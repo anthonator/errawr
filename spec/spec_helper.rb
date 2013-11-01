2ba9becd20119f372cd3f269bc0c8ae455046869
Anthony Smith <anthony@sticksnleaves.com>
2013-11-01 17:34:38 -0400

    Configured rspec.

---
+++ spec/spec_helper.rb
@@ -0,0 +1,8 @@
+require 'coveralls'
+Coveralls.wear!
+
+require 'errawr'
+
+# Require supporting ruby files with custom matchers and macros, etc,
+# in spec/support/ and its subdirectories.
+Dir["#{File.dirname(__FILE__)}/support/**/*.rb"].each { |f| puts f; require f }
