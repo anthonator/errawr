ebb566a386ebaa98716cdef9b55880a59f0a8c63
Anthony Smith <anthony@sticksnleaves.com>
2013-11-20 14:26:49 -0500

    Modified the Errawr module to allow inclusion of ClassMethods.

--- lib/errawr.rb
+++ lib/errawr.rb
@@ -8,14 +8,21 @@ module Errawr
   I18n.load_path += Dir.glob('lib/errawr/locales/*.{rb,yml}')
   I18n.reload!
   
-  def self.error!(name, context = {})
-    klass = Mapper[name] || Mapper[:unknown]
-    klass.metadata.merge!(context.delete(:metadata) || {})
-    klass.context.merge!(context)
-    raise klass
+  def self.included(base)
+    base.extend(ClassMethods)
   end
   
-  def self.register!(key, options = {})
-    Mapper.register!(key, options)
+  module ClassMethods
+    def error!(name, context = {})
+      klass = Mapper[name] || Mapper[:unknown]
+      klass.metadata.merge!(context.delete(:metadata) || {})
+      klass.context.merge!(context)
+      raise klass
+    end
+
+    def register!(key, options = {})
+      Mapper.register!(key, options)
+    end
   end
+  extend ClassMethods
 end
